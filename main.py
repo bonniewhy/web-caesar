7ff5c948bd75804b92b90ad685ed1a76ce11f41a
bonniewhy <bonnieinpixels@gmail.com>
2018-10-12 08:45:11 -0500

    Can't believe I'm actually done... something's wrong...

--- main.py
+++ main.py
@@ -9,25 +9,26 @@ form = """
     <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form action"/" method="POST">
-        <input type="text" name="rot" value="0">
-        <textarea name="text"></textarea>
+        <label for="rot">Rotate by: 
+        <input type="text" name="rot" value="0"></label>
+        <textarea name="text" value="">{0}</textarea>
         <input type="submit">
         </form>
     </body>
@@ -39,10 +40,9 @@ def encrypt():
     text = request.form['text']
     encrypted = rotate_string(text, rot)
 
-    return "<h1>" + encrypted + "</h1>"
+    return form.format(encrypted)
 
 @app.route("/")
 def index():
-    return form
-
+    return form.format("")
 app.run()
